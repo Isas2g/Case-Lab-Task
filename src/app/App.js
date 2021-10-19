3b71ec717a6f08d3a6e4d916c1b7c972e12e875e
thNel <nel.alexen@gmail.com>
2021-10-19 17:55:33 +0500

    Separeted to Classes

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from '../header/Header';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className="App">
+          <Header/>
+
+        </div>
+    );
+  }
+}
+
+export default App;
