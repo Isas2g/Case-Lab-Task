183f501452ebae62bf7bfc41867f711702393bc0
Isas2g <parabell241@gmail.com>
2021-10-21 00:13:11 +0300

    added trackdetail class, deleted crud folder

---
+++ src/classes/TrackDetail.js
@@ -0,0 +1,49 @@
+export default class TrackDetail {
+  constructor(id, trackId, finished, assigned, epilogId, epilogFinished, entityName, entityDuration, data) {
+    this.id = id;
+    this.trackId = trackId;
+    this.finished = finished;
+    this.assigned = assigned;
+    this.epilogId = epilogId;
+    this.epilogFinished = epilogFinished;
+    this.entityName = entityName;
+    this.entityDuration = entityDuration;
+    this.data = data;
+  }
+  
+  get id() {
+    return this.id;
+  }
+  
+  get trackId() {
+    return this.trackId;
+  }
+  
+  get finished() {
+    return this.finished;
+  }
+  
+  get assigned() {
+    return this.assigned;
+  }
+  
+  get epilogId() {
+    return this.epilogId;
+  }
+  
+  get epilogFinished() {
+    return this.epilogFinished;
+  }
+  
+  get entityName() {
+    return this.entityName;
+  }
+  
+  get entityDuration() {
+    return this.entityDuration;
+  }
+  
+  get data() {
+    return this.data;
+  }
+}
