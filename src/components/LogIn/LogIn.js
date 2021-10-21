8d012d921138005f9d1da1195ab9a55e909efced
Sableuze <everglow99@mail.ru>
2021-10-21 20:34:16 +0300

    Удалил обычный bootstrap, заменив " react-bootstrap". Поменял кнопки авторизации

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,52 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import {connect} from "react-redux";
-import {loginTeacher, loginStudent} from "../../actions/auth";
-import {Redirect, Router} from "react-router-dom";
+import { connect } from "react-redux";
+import { loginTeacher, loginStudent } from "../../actions/auth";
+import { Redirect, Router } from "react-router-dom";
+
+import { Button, ButtonGroup } from "react-bootstrap";
 
 class LogIn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.wrapper = React.createRef();
-        this.student.bind(this);
-        this.teacher.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.wrapper = React.createRef();
+    this.student.bind(this);
+    this.teacher.bind(this);
+  }
 
-    student = () => {
-        const { dispatch, history } = this.props;
-        localStorage.setItem('token', '00k488okc8c804ocw8w80s4c0kg8g4ow04ssgk4k')
-        dispatch(loginStudent());
-        // return <Router history={history} ><Redirect to="/" /></Router>
-    }
+  student = () => {
+    const { dispatch, history } = this.props;
+    localStorage.setItem("token", "00k488okc8c804ocw8w80s4c0kg8g4ow04ssgk4k");
+    dispatch(loginStudent());
+    // return <Router history={history} ><Redirect to="/" /></Router>
+  };
 
-    teacher = () => {
-        const { dispatch, history } = this.props;
-        localStorage.setItem('token', 'ks8w8wow40sw8sw0o4wwo0c40s4000wgo8gg4os4')
-        dispatch(loginTeacher());
+  teacher = () => {
+    const { dispatch, history } = this.props;
+    localStorage.setItem("token", "ks8w8wow40sw8sw0o4wwo0c40s4000wgo8gg4os4");
+    dispatch(loginTeacher());
 
-        // return <Router history={history}><Redirect to="/" /></Router>
-    }
+    // return <Router history={history}><Redirect to="/" /></Router>
+  };
 
 
-    render() {
-        return(
-            <>
-                <ul>
-                    <li>
-                        <a href="/" onClick={this.student}> Log In As Student </a>
-                    </li>
-                    <li>
-                        <a href="/" onClick={this.teacher}> Log In As Teacher </a>
-                    </li>
-                </ul>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <ButtonGroup className="gap-3" size="lg">
+          <Button href="/" variant={"success"} onClick={this.student}> Log In As Student </Button>
+          <Button href="/" variant={"primary"} onClick={this.teacher}> Log In As Teacher </Button>
+        </ButtonGroup>
+      </>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-    };
+  return {};
 }
 
 export default connect(mapStateToProps)(LogIn);
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
