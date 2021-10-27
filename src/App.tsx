cead03d65f95e0934c7d37755b91ef9c5a4e415b
Isas2g <parabell241@gmail.com>
2021-10-27 16:21:01 +0300

    rewrote app and index in tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import {observer} from 'mobx-react-lite';
-import {BrowserRouter as Router, Switch} from "react-router-dom";
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import './assets/App.scss';
 
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Tracks } from './components/Tracks';
+import { CreateTrack } from './components/CreateTrack';
+import { GetTrack } from './components/GetTrack';
+import { Login } from './components/Login';
+import { Error } from './components/Error';
+
 export const App: React.FC = observer(() => {
+  
+  const token = localStorage.getItem('token');
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+          <Route exact path="/tracks" component={Tracks}>
+              {token ? <Route exact path="/tracks" component={Tracks} />:<Redirect to="/login" />}
+          </Route>
+          <Route exact path="/new/track" component={CreateTrack}>
+              {token ? <Route exact path="/new/track" component={CreateTrack} />:<Redirect to="/login" />}
+          </Route>
+          <Route exact path="/tracks/:id" component={GetTrack}>
+              {token ? <Route exact path="/tracks/:id" component={GetTrack} />:<Redirect to="/login" />}
+          </Route>
+          <Route exact path="/login" component={Login} />
+          <Route component={Error}>
+              {token ? <Route component={Error} />:<Redirect to="/login" />}
+          </Route>
+      </Switch>
+      <Footer />
+  </Router>
   );
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.scss';
-import App from './App';
+import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
