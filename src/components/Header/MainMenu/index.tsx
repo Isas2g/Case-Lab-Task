a5ca120c5cf85efd0ce640c71a693654ce7959a4
Гусев Алексей <nel.alexen@gmail.com>
2021-10-29 13:23:03 +0500

    A bit of refactoring

--- src/components/Header/MainMenu/index.tsx
+++ src/components/Header/MainMenu/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import 'bootstrap/dist/js/bootstrap.bundle';
 import notificationIcon from './notification-icon.png';
 import userIcon from './user-icon.png';
 
@@ -13,12 +13,35 @@ export const MainMenu = () => {
                 <Link to='/tracks'><p className="nav-link">Каталог</p></Link>
             </nav>
             <div className="d-flex justify-content-between align-items-center">
-                <div className="notifications">
-                    <img width="40" src={notificationIcon} alt="" />
+                <div className="card p-3">
+                    <a className="link-unstyled dropdown" href="#" id="profileScrollingDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        <div className="d-flex align-items-center">
+                            <div className="image">
+                                <img className="m-1 rounded" height="40" width="40" src={userIcon} alt="User's Avatar" />
+                            </div>
+                            <div className="ml-3 w-100">
+                                <h4 className="mb-0 mt-0">[Имя пользователя]</h4>
+                            </div>
+                        </div>
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="profileScrollingDropdown">
+                        <li>
+                            <Link to="/profile" className="dropdown-item">Мой профиль</Link>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Another action</a>
+                        </li>
+                        <li>
+                            <hr className="dropdown-divider" />
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                        </li>
+                    </ul>
                 </div>
-                <div>
-                    <img className="m-1"  width="40" src={userIcon} alt="" />
-                    <Link to="/profile">Иван Иванов</Link>
+                <div className="notifications" style={{marginLeft: 40,}}>
+                    <img width="40" src={notificationIcon} alt="Notification Bell" />
                 </div>
             </div>
         </div>
--- src/modules/Profile/index.tsx
+++ src/modules/Profile/index.tsx
@@ -14,7 +14,9 @@ export const Profile = () => {
   
   return (
     <div className="container d-flex">
-      <div className="m-2" style={{width: 200, height: 200, background: "#f0f0f0"}}></div>
+      <div className="m-2" style={{width: 200, height: 200, background: "#f0f0f0"}}>
+          <img style={{width: 200, height: 200}} src="[url]" />
+      </div>
       <div>
         <p>Role: {role}</p>
         <button onClick={leave} className="btn btn-primary">Выйти из аккаунта</button>
