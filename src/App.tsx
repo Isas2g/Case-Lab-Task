0ed3f87c1bc3d43187cd0a3de187ae38bafa49ee
Isas2g <parabell241@gmail.com>
2021-10-29 10:46:00 +0300

    fixed login issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {observer} from 'mobx-react-lite';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './assets/App.scss';
@@ -8,28 +8,31 @@ import Header from './components/Header';
 import Tracks from './modules/Tracks';
 import CreateTrack from './modules/Tracks/components/CreateTrack';
 import GetTrack from './modules/Tracks/components/GetTrack';
-import Login from './modules/Login';
+import { Login } from './modules/Login';
 import Error from './components/Error';
 import UpdateTrack from "./modules/Tracks/components/UpdateTrack";
 import { Main } from './modules/Main';
 import { Profile } from './modules/Profile';
 
 export const App: React.FC = observer(() => {
+  const [token, setToken] = useState('');
   
-  const token = localStorage.getItem('token');
+  const tokenCode = localStorage.getItem('token');
+  
+  const loginPage = (props: any) => <Login token={token} setToken={setToken} />
   
   return (
     <Router>
       {token ? <Header /> : ''}
       <Switch>
-        <Route exact path="/" component={token ? Main : Login} />
-        <Route exact path="/profile" component={token ? Profile : Login} />
-        <Route exact path="/tracks" component={token ? Tracks : Login} />
-        <Route exact path="/tracks/new" component={token ? CreateTrack : Login} />
-        <Route exact path="/tracks/:id" component={token ? GetTrack : Login} />
-        <Route exact path="/tracks/edit/:id" component={token ? UpdateTrack : Login} />
-        <Route exact path="/login" component={Login} />
-        <Route component={token ? Error : Login} />
+        <Route exact path="/" component={token ? Main : loginPage} />
+        <Route exact path="/profile" component={token ? (props: any) => <Profile token={token} setToken={setToken} /> : loginPage} />
+        <Route exact path="/tracks" component={token ? Tracks : loginPage} />
+        <Route exact path="/tracks/new" component={token ? CreateTrack : loginPage} />
+        <Route exact path="/tracks/:id" component={token ? GetTrack : loginPage} />
+        <Route exact path="/tracks/edit/:id" component={token ? UpdateTrack : loginPage} />
+        <Route exact path="/login" component={loginPage} />
+        <Route component={token ? Error : loginPage} />
       </Switch>
     </Router>
   );
