a443806170b7995e4b002cf6495e574355573b9d
DaryaSerikova <dvserikova@gmail.com>
2021-11-01 22:53:22 +0700

    add file index.tsx

---
+++ src/modules/Profile/style/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+    src: string;
+}
+
+const AvatarImage = styled.img`
+    width: 200px;
+    height: 200px;
+
+`
+const AvatarDiv = styled.div`
+    width: 200px; 
+    height: 200px; 
+    background: #f0f0f0;
+
+`
+
+const Avatar: React.FC<Props> = ({src}) => {
+    return(<AvatarDiv className="m-2">
+        <AvatarImage src={src} />
+    </AvatarDiv>)
+}
+
+export default Avatar;
