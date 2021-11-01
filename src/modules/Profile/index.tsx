6786c47dec57bf258c0ff6b59f255c0017e49b20
DaryaSerikova <dvserikova@gmail.com>
2021-11-01 23:01:41 +0700

    forgeted conflict

--- src/modules/Profile/index.tsx
+++ src/modules/Profile/index.tsx
@@ -1,10 +1,6 @@
 import { useHistory } from "react-router";
-<<<<<<< HEAD
-import Avatar from "./style";
-=======
 import {Avatar} from "./style";
 import React from "react";
->>>>>>> 03609474e02f28945c8cbbf9cea71239fa3de63a
 
 export const Profile: React.FC = () => {
   
