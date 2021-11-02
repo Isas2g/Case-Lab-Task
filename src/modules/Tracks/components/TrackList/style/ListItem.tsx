1f8e0856bb63e43a2be04630e7fa9a0d629f1371
DaryaSerikova <dvserikova@gmail.com>
2021-11-03 06:02:16 +0700

    adaptive miniCards

--- src/modules/Tracks/components/TrackList/style/ListItem.tsx
+++ src/modules/Tracks/components/TrackList/style/ListItem.tsx
@@ -5,6 +5,8 @@ import style from "./style.module.scss";
 import { TrackModal } from "../../TrackModal";
 import {Button, Card, Col } from "react-bootstrap";
 import { Modal } from "react-bootstrap";
+import "./style.module.css"
+
 const Cross = styled.b`
     cursor: pointer;
     position: absolute;
@@ -21,7 +23,7 @@ export const ListItem = (props: any) => {
         <>
             <Col>
                 <Card className={"bg-dark text-white" + style.pointer + ' ' + style.bright} onClick={() => setModalShow(true)}>
-                    <Card.Img src={"https://tml10.rosatom.ru/"+props.track.data.previewPicture} alt="Card image" height={230} />
+                    <Card.Img className="cardImage" src={"https://tml10.rosatom.ru/"+props.track.data.previewPicture} alt="Card image" height={230} />
                     <Card.ImgOverlay className={"d-flex align-items-end"}>
                         <Card.Title className={style.contrast}>{props.track.data.name}</Card.Title>
                         {role === "teacher" ? <Cross className="close" onClick={() => store.deleteTrack(props.track)}>✖</Cross> : ""}
